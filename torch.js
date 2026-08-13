efd8160006e63e4a1904bd63924036719a7c1029
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2026-08-13 17:01:06 +0200

    increase timeout
    Added environment variable for increased HTTP timeout for Linux NVIDIA.

--- torch.js
+++ torch.js
@@ -47,6 +47,7 @@ module.exports = {
       "method": "shell.run",
       "params": {
         "bluefairy": "off",
+        "env": { "UV_HTTP_TIMEOUT": "60" },
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall"
